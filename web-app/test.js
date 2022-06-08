d3656bcd8297124966bc198259c78e79c9b72706
neilpatel4 <73247224+neilpatel4@users.noreply.github.com>
2022-06-08 19:27:51 +0100

    v11

--- web-app/test.js
+++
@@ -1,9 +0,0 @@
-const next_button = document.getElementById("Next_button");
-
-console.log(next_button);
-
-next_button.onclick = function () {
-    //Linear acutuator = move down
-    console.log("Linear Actuator moved down");
-};
-
