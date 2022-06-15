c5b1e54800bcd0f7b1b4448da14548039014f0f6
saianp <73165916+saianp@users.noreply.github.com>
2022-06-15 17:55:34 +0100

    test

---
+++ linear-actuator/led-control.js
@@ -0,0 +1,14 @@
+// import `onoff` package
+const { Gpio } = require( 'onoff' );
+
+// configure LED pins
+const LAE = new Gpio( 26, 'out' );
+const LAR = new Gpio( 19, 'out' );
+const pin_blue = new Gpio( 13, 'out' );
+
+// toggle LED states
+exports.toggle = ( r, g, b ) => {
+  LAE.writeSync( r ? 1 : 0 );
+  LAR.writeSync( g ? 1 : 0 );
+  pin_blue.writeSync( b ? 1 :0 );
+};
